55e555a35122020f7a502e62bea51909cb88798c
dstrickl7 <strickl6@illinois.edu>
2021-12-29 17:24:09 -0600

    starting social icon sidebar styling

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector(".hamburger");
 const navlist = document.querySelector(".navlist-container");
 const overlay = document.querySelector(".overlay");
 const navBtn = document.querySelector(".nav-btn");
+const navlinks = document.querySelectorAll(".navitem");
 
 // Open mobile nav
 if (navlist) {
@@ -11,4 +12,21 @@ if (navlist) {
     navlist.classList.toggle("active");
     overlay.classList.toggle("active");
   });
+
+  navlinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      hamburger.click();
+    });
+  });
 }
+
+/***********************************************************/
+// Social icon sidebar variables
+const sidebar = document.querySelector(".icon-bar");
+/***********************************************************/
+
+/***********************************************************/
+
+/***********************************************************/
+
+/***********************************************************/
