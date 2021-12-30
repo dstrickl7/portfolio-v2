449ef4f3f6e24bc819322735b4a9ee6f128e3945
dstrickl7 <strickl6@illinois.edu>
2021-12-30 12:27:29 -0600

    added section observer to display social icon bar on scroll

--- scripts/index.js
+++ scripts/index.js
@@ -23,7 +23,35 @@ if (navlist) {
 /***********************************************************/
 // Social icon sidebar variables
 const sidebar = document.querySelector(".icon-bar");
+const projectSection = document.querySelector("#projects");
+const options = {
+  root: null, //viewport, default null
+  threshold: 0.25, //btwn 0 and 1, default 0 how much of the item is on page
+  rootMargin: "0px", //must be in px or %
+};
+const projectSectionObserver = new IntersectionObserver(
+  (entries, projectSectionObserver) => {
+    entries.forEach((entry) => {
+      sidebar.classList.toggle("visible");
+    });
+  },
+  options
+);
+projectSectionObserver.observe(projectSection);
 /***********************************************************/
+// Social icon sidebar variables
+const projects = document.querySelectorAll(".project");
+const projectOptions = {
+  root: null, //viewport, default null
+  threshold: 0, //btwn 0 and 1, default 0 how much of the item is on page
+  rootMargin: "0px", //must be in px or %
+};
+const projectObserver = new IntersectionObserver((entries, projectObserver) => {
+  entries.forEach((entry) => {});
+}, projectOptions);
+projects.forEach((project) => {
+  projectObserver.observe(project);
+});
 
 /***********************************************************/
 
